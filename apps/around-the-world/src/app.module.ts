588453dd2dc7a184d1f9f59b45e5bb36bab7a3f9
Gammani <shtucer31@gmail.com>
2024-09-04 13:14:21 +0300

    fix

--- apps/around-the-world/src/app.module.ts
+++ apps/around-the-world/src/app.module.ts
@@ -27,7 +27,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..,..,..', 'swagger-static'),
+      rootPath: join(__dirname, '..,..', 'swagger-static'),
       serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
     }),
     TypeOrmModule.forRoot(options),
